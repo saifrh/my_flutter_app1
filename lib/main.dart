20c7069be6393a76d9f931e5cde443fd2b7d7867
saifrh <50573325+saifrh@users.noreply.github.com>
2024-12-30 13:20:35 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to CodeMagic!'),
+        ),
+        body: Center(
+          child: Text('Hello, DartPad + CodeMagic!'),
+        ),
+      ),
+    );
+  }
+}
 
